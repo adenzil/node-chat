9aff024c22bda9e52bed711fef74b7eb24a63504
Alister <alistercabral@gmail.com>
2016-05-07 10:49:16 +0530

    connected mongoldb

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+
+var url = 'mongodb://localhost:27017/chat';
+
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server.");
+//   db.close();
+// });
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -28,8 +39,10 @@ io.on('connection', function(socket){
 
 	socket.on('end',function(name){
 		var index = peopleonline.indexOf(name);
-		peopleonline.splice(index, 1);
-		io.emit('peopleonline', peopleonline);
+		if(index >= 0){
+			peopleonline.splice(index, 1);
+			io.emit('peopleonline', peopleonline);
+		}
 	});
 
 	socket.on('typing',function(name){
@@ -37,6 +50,27 @@ io.on('connection', function(socket){
 	});
 
 });
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  chat(db,'ali','bye', function() {
+      db.close();
+  });
+});
+
+
+var chat = function(db,name,msg, callback) {
+  var content = {};
+  content[name] = [msg,new Date()];
+   db.collection('chat').insertOne( {
+      content[name]
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback();
+  });
+};
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
